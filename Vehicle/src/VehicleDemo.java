d73a29e770f561dc4c19d702ff7405d302527037
baju123 <32977824+baju123@users.noreply.github.com>
2017-10-21 15:17:25 -0400

    vehicles

---
+++ Vehicle/src/VehicleDemo.java
@@ -0,0 +1,16 @@
+
+public class VehicleDemo {
+public static void main(String[] args) {
+	Vehicle v1 = new Vehicle();
+	v1.setVehicleNumber = "NPJS2345";
+	v1.setModelName = "Honda";
+	v1.setCapacity = (200);
+	v1.setColor = ("Red");
+	v1.setManufacturingYear = (2014);
+	
+	System.out.println(v1.toString());
+	
+	
+	
+}
+}
