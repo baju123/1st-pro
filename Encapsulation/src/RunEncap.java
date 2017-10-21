236509a81a325678e8c0ec53ff68699a82ba3c26
baju123 <32977824+baju123@users.noreply.github.com>
2017-10-21 15:04:24 -0400

    Encapsulation

---
+++ Encapsulation/src/RunEncap.java
@@ -0,0 +1,14 @@
+
+public class RunEncap {
+	public static void main(String[] args) {
+		EncapTest encap = new EncapTest();
+
+		encap.setName("James");
+		encap.setAge(30);
+		encap.setidNum("123qwe");
+		encap.setAddress("Jaffna");
+		 System.out.println("Name: " + encap.getName() +  " " + "Age: " + encap.getAge() + " " + "from: " + encap.getAddress() );
+		
+
+}
+}
